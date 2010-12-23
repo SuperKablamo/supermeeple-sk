93719a62e6d27cc971f7bdf249ac6bfdba0af10d
Will Merydith <will.merydith@gmail.com>
2010-12-23 16:53:20 -0600

    Playing with Facebook Python SDK.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-# Request Handlers
+#
+# Copyright 2010 SuperKablamo, LLC
+#
+
+FACEBOOK_APP_ID = "149881721731503"
+FACEBOOK_APP_SECRET = "8e79a7b1a2a58bc4824312094092c03e"
 
 import os
 import cgi
 import freebase
 import logging
+import facebook
+import models
 
 from django.utils import simplejson
 from google.appengine.ext import db
@@ -13,9 +20,42 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 from urlparse import urlparse
 
-import models
 
-class MainHandler(webapp.RequestHandler):
+class BaseHandler(webapp.RequestHandler):
+    """Provides access to the active Facebook user in self.current_user
+
+    The property is lazy-loaded on first access, using the cookie saved
+    by the Facebook JavaScript SDK to determine the user ID of the active
+    user. See http://developers.facebook.com/docs/authentication/ for
+    more information.
+    """
+    @property
+    def current_user(self):
+        if not hasattr(self, "_current_user"):
+            self._current_user = None
+            cookie = facebook.get_user_from_cookie(
+                self.request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+            if cookie:
+                # Store a local instance of the user data so we don't need
+                # a round-trip to Facebook on every request
+                user = models.User.get_by_key_name(cookie["uid"])
+                if not user:
+                    graph = facebook.GraphAPI(cookie["access_token"])
+                    profile = graph.get_object("me")
+                    user = models.User(key_name=str(profile["id"]),
+                                       id=str(profile["id"]),
+                                       name=profile["name"],
+                                       profile_url=profile["link"],
+                                       access_token=cookie["access_token"])
+                    user.put()
+                elif user.access_token != cookie["access_token"]:
+                    user.access_token = cookie["access_token"]
+                    user.put()
+                self._current_user = user
+        return self._current_user
+
+
+class MainHandler(BaseHandler):
     """Return content for index.html.    
     """
     def get(self):
@@ -59,10 +99,10 @@ class MainHandler(webapp.RequestHandler):
             'games': games
         }        
         directory = os.path.dirname(__file__)
-        path = os.path.join(directory, os.path.join('templates', 'index.html'))
+        path = os.path.join(directory, os.path.join('templates', 'base_index.html'))
         self.response.out.write(template.render(path, template_values, debug=True))
  
-class GameProfile(webapp.RequestHandler):
+class GameProfile(BaseHandler):
     """Returns a Game data
     
     GET - uses a guid to look up a Game on Freebase.  The datastore is 
@@ -101,12 +141,15 @@ class GameProfile(webapp.RequestHandler):
         template_values = {
             'game': data,
             'playerMinMax': playerMinMax,
-            'weblink': weblink
+            'weblink': weblink,
+            'current_user': self.current_user,
+            'facebook_app_id': FACEBOOK_APP_ID
         }  
 
         directory = os.path.dirname(__file__)
-        path = os.path.join(directory, os.path.join('templates', 'game.html'))
-        self.response.out.write(template.render(path, template_values, debug=True))    
+        path = os.path.join(directory, os.path.join('templates', 'base_game.html'))
+        self.response.out.write(template.render(path, template_values, debug=True))  
+          
     # Game search POST
     def post(self):
         logging.info('########### GameProfile::post ###########')
@@ -174,14 +217,46 @@ class GameProfile(webapp.RequestHandler):
         }  
 
         directory = os.path.dirname(__file__)
-        path = os.path.join(directory, os.path.join('templates', 'game.html'))
+        path = os.path.join(directory, os.path.join('templates', 'base_game.html'))
         self.response.out.write(template.render(path, template_values, debug=True))
 
-application = webapp.WSGIApplication(
-                                     [('/', MainHandler),
-                                     ('/game-profile', GameProfile),
-                                     (r'/game-profile(/m/.*)', GameProfile)],
-                                     debug=True)
+class GameCheckin(BaseHandler):
+    
+    # Checkin to Game
+    def post(self):
+        logging.info('########### GameCheckin::post ###########')
+        data = getGame(self.request.get('mid'))
+        
+        # Can't access properties with special charactes in Django, so create
+        # a dictionary.
+        playerMinMax = data["/games/game/number_of_players"]
+        weblink = data["/common/topic/weblink"]
+
+        # create/update Game data
+        entity = models.Game.get_by_key_name(data.mid)
+        if not entity:
+            entity = models.Game(key_name=data.mid, name=data.name)
+            logging.info('## CREATING NEW ENTITY: KEY: ' + str(entity.key()) + 
+                         ' | NAME: ' + entity.name)
+                              
+        else:
+            entity.name = data.name            
+            logging.info('## UPDATING ENTITY: KEY: ' + str(entity.key()) + 
+                         ' | NAME: ' + entity.name)
+        
+        entity.put()
+                              
+        template_values = {
+            'game': data,
+            'playerMinMax': playerMinMax,
+            'weblink': weblink
+        }
+        
+        directory = os.path.dirname(__file__)
+        path = os.path.join(directory, os.path.join('templates', 'base_game.html'))
+        self.response.out.write(template.render(path, template_values, debug=True))
+
+######################## METHODS #############################################
 
 def getGame(mid):
     """Returns a Freebase emql result for Game data.    
@@ -214,6 +289,14 @@ def getGame(mid):
     }        
     result = freebase.mqlread(query, extended=True)   
     return result
+    
+##############################################################################
+application = webapp.WSGIApplication(
+                                     [('/', MainHandler),
+                                     ('/game-profile', GameProfile),
+                                     (r'/game-profile(/m/.*)', GameProfile),
+                                     ('/game-checkin', GameCheckin)],
+                                     debug=True)
 
 def main():
     run_wsgi_app(application)
--- models.py
+++ models.py
@@ -3,8 +3,12 @@
 from google.appengine.ext import db
 
 class User(db.Model):
+    id = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    updated = db.DateTimeProperty(auto_now=True)
     name = db.StringProperty(required=True)
-    rating = db.IntegerProperty(required=True)
+    profile_url = db.StringProperty(required=True)
+    access_token = db.StringProperty(required=True)
 
 class Game(db.Model):
     name = db.StringProperty(required=True)
